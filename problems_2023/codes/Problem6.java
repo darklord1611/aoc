45ecf73abf788b38c67f2a27f9c014bf819f1224
darklord1611 <tavietanh16112004@gmail.com>
2023-12-09 20:56:15 +0700

    solve problem6 part 2

--- problems_2023/codes/Problem6.java
+++ problems_2023/codes/Problem6.java
@@ -10,8 +10,8 @@ public class Problem6 {
 
   public static void main(String[] args) throws FileNotFoundException {
     Scanner scanner = new Scanner(new File("problems_2023/inputs/problem6_input.txt"));
-    List<Integer> times = new ArrayList<>();
-    List<Integer> dist = new ArrayList<>();
+    List<Long> times = new ArrayList<>();
+    List<Long> dist = new ArrayList<>();
     List<String> lines = new ArrayList<>();
 
     while(scanner.hasNext()) {
@@ -19,26 +19,30 @@ public class Problem6 {
     }
 
     for(String part : lines.get(0).split(" ")) {
-      times.add(Integer.parseInt(part));
+      times.add(Long.parseLong(part));
     }
 
     for(String part : lines.get(1).split(" ")) {
-      dist.add(Integer.parseInt(part));
+      dist.add(Long.parseLong(part));
     }
 
 
-    int total = 1;
+    long index1 = 0;
+    long index2 = 0;
     for(int i = 0; i < times.size(); i++) {
-      int cnt = 0;
-      for(int j = 0; j < times.get(i); j++) {
-        if((times.get(i) - j) * j > dist.get(i)) {
-          cnt++;
+      for(long j = 0; j < times.get(i); j++) {
+        if(j * (times.get(i) - j) > dist.get(i)) {
+          index1 = times.get(i) - j;
         }
       }
 
-      total *= cnt;
+      for(long j = times.get(i) - 1; j >= 0; j--) {
+        if((times.get(i) - j) * j > dist.get(i)) {
+          index2 = times.get(i) - j;
+        }
+      }
     }
-
-    System.out.println(total);
+    System.out.println(index1 + " " + index2);
+    System.out.println(index2 - index1 + 1);
   }
 }
